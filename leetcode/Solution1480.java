4baa690d29c825d487b035eec90d09d3f8344f4d
alun2kb <k5730@foxmail.com>
2020-08-12 21:54:49 +0800

    Create Solution1480.java
    slove 1480

---
+++ leetcode/Solution1480.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int[] runningSum(int[] nums) {
+        int[] rtn = new int[nums.length];
+        rtn[0] = nums[0];
+        for(int i = 1; i < nums.length; i++)
+            rtn[i] = rtn[i - 1] + nums[i];
+        return rtn;
+    }
+}
