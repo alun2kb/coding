bfd72904b621d5a4ee74eedc818a149be741f3c4
alun2kb <k5730@foxmail.com>
2020-08-16 23:51:42 +0800

    Update Solution733.java

--- leetcode/Solution733.java
+++ leetcode/Solution733.java
@@ -1,1 +1,20 @@
+public class Solution {
 
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        if(image[sr][sc] != newColor)
+            dfs(image, sr, sc, image[sr][sc], newColor);
+        return image;
+    }
+
+    public void dfs(int[][] image, int x, int y, int judge, int newColor){
+        if(x < 0 || y < 0 || x >= image.length || y >= image[0].length || image[x][y] != judge || image[x][y] == newColor)
+            return;
+        image[x][y] = newColor;
+        //print(image);
+        dfs(image, x + 1, y, judge, newColor);
+        dfs(image, x - 1, y, judge, newColor);
+        dfs(image, x, y + 1, judge, newColor);
+        dfs(image, x, y - 1, judge, newColor);
+        return;
+    }
+}
